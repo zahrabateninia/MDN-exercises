382f369711ca3bbadc282491f2c0b56a4a106b33
zahrabateninia <zahrabatenin@gmail.com>
2023-11-12 11:19:18 +0330

    Set up an eventListener of "Close" button.

--- dialog/index.js
+++ dialog/index.js
@@ -2,9 +2,11 @@ const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button"); // select the button that comes immediately after the dialog element
 const closeButton = document.querySelector("dialog button"); // select the button that is inside the dialog
 
-
-// Event listener for the "Show the dialog" button
 showButton.addEventListener("click", () => {
     // Open the dialog modally
     dialog.showModal();
+  });
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
   });
