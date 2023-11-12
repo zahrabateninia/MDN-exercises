d7f3b0556619532a83425e05368b9a6bb1584eff
zahrabateninia <zahrabatenin@gmail.com>
2023-11-12 11:15:58 +0330

    Set up an eventListener on the "show the dialog" button.
    When this button is clicked, it triggers the showModal() method on the dialog element, which opens the dialog modally.
    Modally means that it blocks interactions with the rest of the page until the dialog is closed.

--- dialog/index.js
+++ dialog/index.js
@@ -1,3 +1,10 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button"); // select the button that comes immediately after the dialog element
 const closeButton = document.querySelector("dialog button"); // select the button that is inside the dialog
+
+
+// Event listener for the "Show the dialog" button
+showButton.addEventListener("click", () => {
+    // Open the dialog modally
+    dialog.showModal();
+  });
